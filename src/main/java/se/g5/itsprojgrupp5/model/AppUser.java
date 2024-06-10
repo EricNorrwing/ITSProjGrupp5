30603f1a678b279fe5a70797aca3535cc7bad1cb
EricNorrwing <eric.norrwing@gmail.com>
2024-06-10 19:55:00 +0200

    Added DTO and user packages

--- src/main/java/se/g5/itsprojgrupp5/model/AppUser.java
+++ src/main/java/se/g5/itsprojgrupp5/model/AppUser.java
@@ -1,6 +1,76 @@
 package se.g5.itsprojgrupp5.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class AppUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String secret;
+
+    private String role;
+
+
+    public AppUser() {
+    }
+
+
+    public AppUser(String email, String password, String secret) {
+        this.email = email;
+        this.password = password;
+        this.secret = secret;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
 }
