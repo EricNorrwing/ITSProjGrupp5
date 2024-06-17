bfae58545cd67a1893925959ccdeda95a3b7d6ae
EricNorrwing <eric.norrwing@gmail.com>
2024-06-17 11:19:06 +0200

    Added post test, but have to wait for Controller methods update

--- src/test/java/se/g5/itsprojgrupp5/PostControllerTest.java
+++ src/test/java/se/g5/itsprojgrupp5/PostControllerTest.java
@@ -1,11 +1,5 @@
 package se.g5.itsprojgrupp5;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,27 +7,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-public class GetControllerTest {
+public class PostControllerTest {
+
 
     @Autowired
     private MockMvc mvc;
-
-    @Test
-    @DisplayName("testing that / cannot be access unless authorized")
-    public void assertThatUserHasToBeLoggedIn() throws Exception {
-        mvc.perform(get("/").with(httpBasic("user", "password")))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    @DisplayName("Asserting that get /admin can be accessed by ADMIN user")
-    public void assertThatAdminUserCanAccessAdminPage() throws Exception {
-        mvc.perform(get("/admin/page").with(httpBasic("admin@admin.se", "adminpass")))
-                .andExpect(status().isOk());
-    }
-
     @Test
     @DisplayName("Testing that a registration will fail when incorrect input is sent")
     public void testFailedRegistration() throws Exception {
@@ -48,7 +33,4 @@ public class GetControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeHasFieldErrors("userDTO", "email", "password", "role"));
     }
-
-
-
 }
